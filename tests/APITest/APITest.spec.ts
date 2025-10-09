07f925aa018d8f14d22eb4d9a41a83964a80fe04
Andres Hernandez <andrestesterqa@gmail.com>
2025-10-09 19:28:29 -0300

    Fix API request URL and correct variable usage in issue retrieval

--- tests/APITest/APITest.spec.ts
+++ tests/APITest/APITest.spec.ts
@@ -4,16 +4,14 @@ const REPO = 'Playwright_Freerange';
 const OWNER =  'andrestesterqa';
 
 test('API Test - Create an issue in a repository', async ({ request }) => {
-    const newIssue = await request.post(`/repos/${OWNER
-        
-    }/${REPO}/issues`, {
+    const newIssue = await request.post(`https://api.github.com/repos/${OWNER}/${REPO}/issues`, {
         data: {
             title: '[Bug] Issue created by Playwright',
             body: 'This is the description of the issue',}
     });
     expect(newIssue.status()).toBe(201);
 
-    const issue = await request.get(`/repos/${USER}/${REPO}/issues`).then(res => res.json());
+    const issue = await request.get(`/repos/${OWNER}/${REPO}/issues`).then(res => res.json());
     expect(issue.ok()).toBeTruthy();
     expect(issue.title).toBe('[Bug] Issue created by Playwright');
     expect(issue.body).toBe('This is the description of the issue');
